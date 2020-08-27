ced24e9505e5163290068e49c9c9e8c706610102
aramtantushyan <60556457+aramtantushyan@users.noreply.github.com>
2020-08-27 05:24:53 +0400

    Update redirect.helper.js

--- src/js/helpers/redirect.helper.js
+++ src/js/helpers/redirect.helper.js
@@ -6,7 +6,7 @@ function redirectToOtherPage (url) {
 
 function redirectToViewedCountry (country) {
     setViewedCountryData(country);
-    redirectToOtherPage('/Homework 19/src/pages/viewCountry.html');
+    redirectToOtherPage('/src/pages/viewCountry.html');
 }
 
 export {redirectToOtherPage};
